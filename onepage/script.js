57ce7fa8d73dc2a27cd10e733ca4fdf349b9551c
elderflower95 <74299226+elderflower95@users.noreply.github.com>
2021-01-12 15:13:20 +0100

    Log in

---
+++ onepage/script.js
@@ -0,0 +1,7 @@
+//kod till inloggning, namnet följer med till onepage.html
+function load() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get('uname');
+    document.getElementById("uname").innerHTML = username.toUpperCase();
+
+}
